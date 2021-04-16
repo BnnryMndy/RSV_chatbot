c32b4edea25bc1971abb8f9d5003626d94df75c3
AntonLazarev <bnnrymndy@gmail.com>
2021-04-17 02:06:14 +0300

    patch script constructor

--- js/createScript.js
+++ js/createScript.js
@@ -1,19 +1,21 @@
+function addAwnser(event) {
+    var awnser = document.createElement("div");
+    awnser.innerHTML = '<fieldset class="awnser" id=""> <legend> Добавление ответа </legend><input placeholder = "Тип реакции" name = "ReactionType" list = "ReactionType"><input placeholder = "реакция" name = "reaction" type = "text"><input type="text" name="awnser" placeholder="ответ"></fieldset>';
+    this.before(awnser);
+}
+
+function addHandler() {
+    $('.add-awnser').on("click", addAwnser);
+}
+
 $(document).ready(function() {
-    // var awnsers = 1;
 
-    // $('.awnser.delete').on("click", function(event) {
-    //     event.target
-    // });
-
-    //
-
-    $('.task #add-awnser').on("click", function(event) {
-        // var AddAwnserButton = $(event);
-        // console.log("clicked: " + event.target.nodeName);
-
-        var awnser = document.createElement("div");
-        awnser.innerHTML = '<fieldset class="awnser" id=""> <legend> Добавление ответа </legend><input placeholder = "Тип реакции" name = "ReactionType" list = "ReactionType"><input placeholder = "реакция" name = "reaction" type = "text"><input type="text" name="awnser" placeholder="ответ"></fieldset>';
-        this.before(awnser);
+    $('.add-awnser').on("click", addAwnser);
+    $(' #add-question').on("click", function(event) {
+        var question = document.createElement("div");
+        question.innerHTML = '<fieldset class="task" id="1"><legend>Создание нового вопроса</legend><input type="text" name="question" placeholder="Вопрос"> <fieldset class="awnser" id=""> <legend> Добавление ответа </legend><input placeholder = "Тип реакции" name = "ReactionType" list = "ReactionType"><input placeholder = "реакция" name = "reaction" type = "text"><input type="text" name="awnser" placeholder="ответ"></fieldset> <input class="add-awnser" type="button" value="+ ответ"></fieldset>';
+        this.before(question);
+        addHandler();
     });
 
 });
