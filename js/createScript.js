c5572a01a775272bc1dc80e4628e9d8ff028a77f
AntonLazarev <bnnrymndy@gmail.com>
2021-04-17 16:57:22 +0300

    added ajax forms

--- js/createScript.js
+++ js/createScript.js
@@ -10,10 +10,11 @@ function addHandler() {
 
 $(document).ready(function() {
 
-    $('.add-awnser').on("click", addAwnser);
-    $(' #add-question').on("click", function(event) {
+    addHandler();
+    // $('.add-awnser').on("click", addAwnser);
+    $('#add-question').on("click", function(event) {
         var question = document.createElement("div");
-        question.innerHTML = '<fieldset class="task" id="1"><legend>Создание нового вопроса</legend><input type="text" name="question" placeholder="Вопрос"> <fieldset class="awnser" id=""> <legend> Добавление ответа </legend><input placeholder = "Тип реакции" name = "ReactionType" list = "ReactionType"><input placeholder = "реакция" name = "reaction" type = "text"><input type="text" name="awnser" placeholder="ответ"></fieldset> <input class="add-awnser" type="button" value="+ ответ"></fieldset>';
+        question.innerHTML = '<fieldset class="task" id="' + '"><legend>Создание нового вопроса</legend><input type="text" name="question" placeholder="Вопрос"> <fieldset class="awnser" id=""> <legend> Добавление ответа </legend><input placeholder = "Тип реакции" name = "ReactionType" list = "ReactionType"><input placeholder = "реакция" name = "reaction" type = "text"><input type="text" name="awnser" placeholder="ответ"></fieldset> <input class="add-awnser" type="button" value="+ ответ"></fieldset>';
         this.before(question);
         addHandler();
     });
